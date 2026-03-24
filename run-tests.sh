b12edb2f73282da4d4bcaa3f330ad7c32841ea81
Fleurdel34 <betdevelweb0953@gmail.com>
2026-03-24 08:07:50 +0100

    add script bash to run tests

--- run-tests.sh
+++ run-tests.sh
@@ -4,7 +4,6 @@ rm -rf test-results/
 rm -f *.log
 rm -rf dist/
 
-
 #Angular
 if [ -f "package.json" ]; then
     echo "Angular project"
@@ -23,5 +22,32 @@ if [ -f "package.json" ]; then
     exit 0
 fi
 
-echo "Nothing Angular project"
-exit 1
+#Java
+if [ -f "build.gradle" ]; then
+    echo "Java project"
+
+    echo "Run tests"
+    ./gradlew clean test
+    status=$?
+
+    if [ $status -ne 0 ]; then
+        echo "Tests failed"
+        exit 1
+    fi
+
+    echo "Copy JUnit reports"
+    mkdir -p test-results
+    cp -r build/test-results/test/* test-results/
+
+    echo "Tests successfully"
+    echo "Report in test-results"
+    exit 0
+fi
+
+#absence of a project
+if [ ! -f "package.json" ] && [ ! -f "build.gradle" ]; then
+    echo "No Angular or Java project found"
+    exit 1
+fi
+
+exit 0
